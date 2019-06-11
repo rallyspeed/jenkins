0c279cb7c432d37c5bd6d0e7a9e09f795a206ec3
Mathieu Durand <28907836+rallyspeed@users.noreply.github.com>
2019-06-11 15:21:42 -0400

    Update runUAT.sh

--- mobile/test/runUAT.sh
+++ mobile/test/runUAT.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 # set variables
-hostname='localhost'
-port=$1
+url=$1
 
 # wait for the app to start
 sleep 5 
 
 # ping the app
-status_code=$(curl --write-out %{http_code} --out /dev/null --silent ${hostname}:${port})
+status_code=$(curl --write-out %{http_code} --out /dev/null --silent ${url})
 
 if [ $status_code == 200 ];
 then
-	echo "PASS: ${hostname}:${port} is reachable"
+	echo "PASS: ${url} is reachable"
 else
-	echo "FAIL: ${hostname}:${port} is unreachable"
+	echo "FAIL: ${url} is unreachable"
     exit 1
 fi
