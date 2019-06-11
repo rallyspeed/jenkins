ebf202623739af4f3098ce62da9984086e421e16
Mathieu Durand <28907836+rallyspeed@users.noreply.github.com>
2019-06-11 09:47:30 -0400

    Update docker_commands.sh

--- mobile/docker_commands.sh
+++ mobile/docker_commands.sh
@@ -1,7 +1,7 @@
     
 # prefix commands with sudo if user is not in docker group
 # pull docker image
-docker pull opensecurity/mobile-security-framework-mobfsdocker
+docker pull opensecurity/mobile-security-framework-mobfs
 
 # run docker container
 docker run -p 8888:5000 --name mobfs mobile-security-framework-mobfsdocker:latest 
